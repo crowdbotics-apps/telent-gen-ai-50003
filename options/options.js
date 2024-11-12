d0082f6fc48622a9507118beeb9e90d29d5b69dc
Crowdbotics <team@crowdbotics.com>
2024-11-12 11:49:01 +0000

    Initial commit

---
+++ options/options.js
@@ -0,0 +1,15 @@
+import { StyleSheet } from "react-native"
+
+export const globalOptions = {
+  name: "telent-gen-ai-50003",
+  url: "https://telent-gen-ai-50003.botics.co",
+  api: "https://telent-gen-ai-50003.botics.co/api/v1",
+  screenOptions: {
+    headerShown: false
+  }
+}
+export const modulesOptions = {
+  "@modules/app-menu": {
+    copy: "Routes available!"
+  }
+}
